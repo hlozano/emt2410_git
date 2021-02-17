61e674cf95d194105c205e16365cd945c6f766d0
hernanatcladdagh <hernan@claddaghelectronics.com>
2021-02-17 18:38:30 -0500

    some changes

--- projects/class_nucleo/class03_blink_timer.cpp
+++ projects/class_nucleo/class03_blink_timer.cpp
@@ -4,14 +4,17 @@ Ticker toggle_led_ticker;
 
 DigitalOut led1(LED1);
 
-void toggle_led() {
+void toggle_led() 
+{
     led1 = !led1;
 }
 
-int main() {
+int main() 
+{
     // Init the ticker with the address of the function (toggle_led) to be attached and the interval (100 ms)
     toggle_led_ticker.attach(&toggle_led, 0.1);
-    while (true) {
+    while (true) 
+    {
         // Do other things...
     }
 }
