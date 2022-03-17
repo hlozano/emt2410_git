1ad8e32fbbaeb1deda331d97ccd54310a0e5402a
hernanatcladdagh <hernan@claddaghelectronics.com>
2022-03-17 10:18:52 -0400

    updates for new compiler

--- projects/class_nucleo/class01_blink_led.cpp
+++ projects/class_nucleo/class01_blink_led.cpp
@@ -1,24 +1,29 @@
 //example of a blinking led
-
 /*
-this
-is 
-a 
+this is a 
 multiline
 comment
 */
 
 #include "mbed.h"
-
+#define SLEEP_TIME_MS 1000 
 DigitalOut myled(LED1);
 
 int main() 
 {
+    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
     while(1) //1 == 1 is a boolean expression -> true
     {
+        myled = 1; // LED is ON
+        thread_sleep_for(SLEEP_TIME_MS ); // 1 sec 
+        myled = 0; // LED is OFF
+        thread_sleep_for(SLEEP_TIME_MS ); // 1 sec
+/*
+        //older version
         myled = 1; // LED is ON
         wait(1.0); // 1 s
         myled = 0; // LED is OFF
         wait(1.0); // 1 sec
+*/        
     }
 }
