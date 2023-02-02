190dd50d61d3cc54fb6c00769688b53e49cece9d
hernanatcladdagh <hernan@claddaghelectronics.com>
2023-02-01 20:54:48 -0500

    more files

---
+++ projects/class_programming_c/class03a_input_output_example.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+   char c;
+   printf("Enter character: ");
+   c = getc(stdin);
+   printf("Character entered: ");
+   putc(c, stdout);
+   printf("\n"); 
+   printf("%d",c);
+   return(0);
+}
