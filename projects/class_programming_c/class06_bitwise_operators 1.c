40a88e307e06fd5cbac73df3c9e4367a0fab21e8
hernanatcladdagh <hernan@claddaghelectronics.com>
2023-03-08 19:40:49 -0500

    updated bitwise-operators

--- projects/class_programming_c/class06_bitwise_operators 1.c
+++ projects/class_programming_c/class06_bitwise_operators 1.c
@@ -15,21 +15,32 @@ int main()
   b = a | 0x81;
   c = a & 0xE7;
   d = a & ~(0x18);
-  
-  e = 0x02; //0b 0000 0010
-            // in order to set this one bit
-            //0b 0100 0000
-            //use the OR bitwise
-  e = e | 0x40;  // e=0b0100 0010
+  // part 1 - how to set a bit
+  e = e | 0x02;  // 0 OR 2 
+            //0b 0000 0000 <-all bits off
+            //0b 0000 0010 <-bit to be turned on
+            //0b 0000 0010 <- result is e with bit 2 on 
+  e = e | 0x40;
+            //0b 0000 0010 <-e starting at
+            //0b 0100 0000 <-bit to be turned on
+            //0b 0100 0010 <- result is e with bit 7 and 2 on 
 
   //e=0x42
-  //in order to clear bit 2... meaning 0x02 use AND bitwise
-  e = e & ~(0x02); // use and  ->  1111 1101 
+   // part 2 - how to clear a bit
+     e = e & ~(0x02); // use and  ->  1111 1101 
 
+    //  ~(0x02) ->  ~(0b 0000 0010)
+    //                0b 1111 1101
+
+     // so this becomes
+            //0b 0100 0010 <-e starting at
+            //0b 1111 1101 <-bit to be turned off
+            //0b 0100 0000 <- result is e with bit 4 only 
 
   printf("Line 1 - Value of a is %u\n", a);
   printf("Line 2 - Value of b is %u\n", b);
   printf("Line 3 - Value of c is %u\n", c);
   printf("Line 4 - Value of d is %u\n", d);
+  printf("Line 5 - Value of e is %u\n", e);
   return 1;
 }
