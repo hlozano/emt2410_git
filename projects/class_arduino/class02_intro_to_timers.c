6f956f365d4e4194096d3adc18e2913bcfbb314e
hernanatcladdagh <hernan@claddaghelectronics.com>
2023-09-22 20:13:16 -0400

    programs for timers were updated

--- projects/class_arduino/class02_intro_to_timers.c
+++ projects/class_arduino/class02_intro_to_timers.c
@@ -4,13 +4,17 @@ unsigned long one_ms_timer = 0; //will help me track one ms intervals
 								// this one keeps track of one ms intervals
 								// it changes from 0 -> 999
 unsigned long counter = 0;
+unsigned long one_sec_counter = 0;
+const int myled = 13;
 void setup()
 {
     Serial.begin(9600);
+    pinMode(myled,OUTPUT);
 }
 void loop()
 {
     counter++;
+  
     if(millis() > ms_runtime)
 	{//goes in this "if" because one millisecond has passed!
 		ms_runtime = ms_runtime + 1;
@@ -20,18 +24,30 @@ void loop()
 
 	 if(one_ms_timer>=1000)
 	//if(one_ms_timer>=100)
-	{	
+	{	one_sec_counter++;
+     
+        if(one_sec_counter % 2 == 1) //Check if one_sec_counter is an ODD number
+          digitalWrite(myled,HIGH); // 5V
+		else
+          digitalWrite(myled,LOW); // 0V
+          
+          
 		one_ms_timer = 0;
-		Serial.print("counter  ");
+		Serial.print("counter ------->  ");
         Serial.println(counter);
+    	
+		Serial.print("one second counter  ");
+        Serial.println(one_sec_counter); 
 
         Serial.print("millis() returns  ");
         Serial.println(millis());
-
-
 		counter = 0;
 	}
 }
+
+
+
+
 
 /* Special Note
 pinted 
